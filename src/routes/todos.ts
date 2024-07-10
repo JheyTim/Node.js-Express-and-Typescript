dcff3baa24b230e25c0f9c1def11f31596436d0f
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-07-11 01:09:56 +0800

    Working with controllers & parsing request bodies

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import { createTodo } from '../controllers/todos';
+
 const router = Router();
 
 router.get('/');
 
-router.post('/');
+router.post('/', createTodo);
 
 router.patch('/:id');
 
